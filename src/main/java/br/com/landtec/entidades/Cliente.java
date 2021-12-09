e7368230c36a98a4213772d57b136f255f72f6f0
Victor Vilar <victor.svilar@gmail.com>
2021-12-09 11:45:13 -0300

    classe Cliente finalizada

--- src/main/java/br/com/landtec/entidades/Cliente.java
+++ src/main/java/br/com/landtec/entidades/Cliente.java
@@ -13,4 +13,17 @@ public class Cliente {
     
     private String nome;
     
+    
+    public Cliente (String nome){
+        this.nome = nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
 }
